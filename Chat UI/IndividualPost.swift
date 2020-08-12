0c00471220fe229fd63a3dc38b32abae2dcaea38
Jasdeep Brar <jasdeepbrar@Jasdeeps-MacBook-Pro.local>
2020-08-11 22:47:32 -0700

    Applied Components to IndividualPost.swift

--- Chat UI/IndividualPost.swift
+++ Chat UI/IndividualPost.swift
@@ -15,21 +15,8 @@ struct IndividualPost: View {
     var body: some View {
         ScrollView {
             VStack {
-                ZStack {
-                    Image("Mount_Rainier_from_west").resizable().frame(width: 415, height: 400)
-                    Button(action: {}) {
-                        Image(systemName: "xmark").resizable().frame(width: 30, height: 30).foregroundColor(Color.white).offset(x: 165, y: -160)
-                    }
-                    Button(action: {}) {
-                        Image(systemName: "chevron.left").resizable().frame(width: 20, height: 30).foregroundColor(Color.white).offset(x: -165, y: 160).opacity(0.5)
-                    }
-                    Button(action: {}) {
-                        Image(systemName: "chevron.right").resizable().frame(width: 20, height: 30).foregroundColor(Color.white).offset(x: 165, y: 160)
-                    }
-                }.padding(.bottom)
-            
+                TopSection()
                 MidSection()
-                
                 CommentsUI()
             }
         }.edgesIgnoringSafeArea(.top)
---
+++ TopSection.swift
@@ -0,0 +1,48 @@
+//
+//  TopSection.swift
+//  Chat UI
+//
+//  Created by Jasdeep Brar on 8/11/20.
+//  Copyright © 2020 Status. All rights reserved.
+//
+
+import SwiftUI
+
+struct TopSection: View {
+    var body: some View {
+        ZStack {
+            Image("Mount_Rainier_from_west").resizable().frame(width: 415, height: 400)
+            Button(action: {}) {
+                Image(systemName: "xmark")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(Color.white)
+            }
+            .position(x: 350, y: 40)
+            HStack{
+                Button(action: {}) {
+                    Image(systemName: "chevron.left")
+                        .resizable()
+                        .frame(width: 20, height: 30)
+                        .foregroundColor(Color.white)
+                        .opacity(0.5)
+                }
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "chevron.right")
+                        .resizable()
+                        .frame(width: 20, height: 30)
+                        .foregroundColor(Color.white)
+                }
+            }
+            .offset(y: 160)
+            .padding(.horizontal, 40)
+        }.padding(.bottom)
+    }
+}
+
+struct TopSection_Previews: PreviewProvider {
+    static var previews: some View {
+        TopSection()
+    }
+}
